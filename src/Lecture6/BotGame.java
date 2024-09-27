f05982f541a814d3224bfe2dee65c0c97482c5c3
tomglove <mrethol@gmail.com>
2024-09-27 15:12:01 +0200

    Del 2 av botgame

--- src/Lecture6/BotGame.java
+++ src/Lecture6/BotGame.java
@@ -1,4 +1,7 @@
 package Lecture6;
+
+import javax.swing.*;
+
 class Robot{
     String navn;
     int batteri;
@@ -7,9 +10,29 @@ class Robot{
         this.navn = navn;
         this.batteri = batteri;
     }
+
+    public void hondterHendelse(String hendelse){
+        switch(hendelse){
+            case "stein":
+                batteri-=10;
+                break;
+            case "batteri":
+                batteri+=20;
+                break;
+            case "menneske":
+                batteri-=30;
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "skriv inn rett da vel, skal være stein eller menneske eller batteri");
+        }
+        JOptionPane.showMessageDialog(null, "du har " + this.batteri +  " batter igjen");
+    }
 }
+
 public class BotGame {
     public static void main(String[] args) {
         Robot robot = new Robot("GlassKanon-9000", 100);
+        String hendelse = JOptionPane.showInputDialog("skriv inn enten stein || mennekse || batteri");
+        robot.hondterHendelse(hendelse);
     }
 }
