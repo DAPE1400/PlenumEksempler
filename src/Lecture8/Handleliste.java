70c5b97b10e4e0f7ad2a8a2efeaa61fc860de4b8
tomglove <mrethol@gmail.com>
2024-10-18 11:26:13 +0200

    Handleliste eksempel

---
+++ src/Lecture8/Handleliste.java
@@ -0,0 +1,19 @@
+package Lecture8;
+
+import java.util.ArrayList;
+
+public class Handleliste {
+    public static void main(String[] args) {
+        String[] handleliste = new String[10];
+        handleliste[1] = "Gummihansker";
+        handleliste[2] = "Sylteagurk";
+        handleliste[3] = "Duct tape";
+        handleliste[4] = "Marshmallows";
+        handleliste[5] = "Hagenisse";
+        handleliste[6] = "Kvikk lønsj";
+        handleliste[7] = "Stor presenning";
+        handleliste[8] = "Ballonger";
+        handleliste[9] = "WD-40";
+        handleliste[10] = "Vekkerklokke";
+    }
+}
